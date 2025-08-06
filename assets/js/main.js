6e62a031ce11ef0c768bd4e1bbfbf438d25b850c
HarshadaSamant <samant.tanuja@gmail.com>
2025-08-05 20:44:57 -0400

    Publications and Certification updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -254,5 +254,18 @@
 			});
 
 		}
+		
+			$(".at-title").click(function () {
+    $(this)
+      .toggleClass("active")
+      .next(".at-tab")
+      .slideToggle()
+      .parent()
+      .siblings()
+      .find(".at-tab")
+      .slideUp()
+      .prev()
+      .removeClass("active");
+  });
 
 })(jQuery);
